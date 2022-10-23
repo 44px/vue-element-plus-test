f9b1fc929f0af1acd45c85887b45d434ce475bed
Alexander Shepelin <brightcor@gmail.com>
2022-10-24 00:10:44 +0400

    Remove unused css

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from "vue";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import "vuetify/styles";
-// import "normalize.css/normalize.css";
-// import "element-plus/dist/index.css";
-import "./style.css";
 import App from "./App.vue";
 import { router } from "./pages/router";
 
